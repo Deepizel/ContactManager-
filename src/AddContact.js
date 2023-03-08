fdfc55ee333fe3ad2ac12cc75c9c09dbe9bc72e7
victor Ekundayo <victor.ekundayo@ietech.com.ng>
2023-03-08 16:53:29 +0100

    added Navbar and Add contact component

---
+++ src/AddContact.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+class AddContact extends React.Component{
+    render(){
+        return (
+          <div>
+            <h2>Add Contact</h2>
+            <form>
+              <div>
+                <div className="mb-3">
+                  <label
+                    htmlFor="exampleFormControlInput1"
+                    className="form-label"
+                  >
+                    Email address
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="name@example.com"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label
+                    htmlFor="exampleFormControlTextarea1"
+                    className="form-label"
+                  >
+                    Example textarea
+                  </label>
+                  <textarea
+                    className="form-control"
+                    id="exampleFormControlTextarea1"
+                    rows={3}
+                    defaultValue={""}
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
+        );
+    }
+}
+
+export default AddContact
--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import AddContact from './AddContact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <AddContact />
+      Hello world!
+      <div>
+        <button className="btn btn-primary mx-4 mt-5">hire me</button>{" "}
+      </div>
     </div>
   );
 }
